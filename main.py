fd37d2481ea359e12a300fbaacafa4e2ed56737a
zyinghua <yzho0149@student.monash.edu>
2023-05-04 15:19:15 +1000

    Formalised and completed prediction pipeline. should be able to lead to predictions

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-from bert_er import *
-from bert_clc import *
+from bert_er import extract_er_result, predict_evi, er_pipeline, CFEVERERTestDataset
+from bert_clc import extract_claim_evi_labels, decide_claim_labels, clc_pipeline, CFEVERLabelTestDataset
 from dataset_loader import load_data
+from torch.utils.data import DataLoader
 
+# ----------------- Prediction -----------------
+loader_batch_size = 24
+loader_worker_num = 2
+gpu = 0
+# ----------------------------------------------
+
+output_filename = '/content/drive/MyDrive/Colab Notebooks/Assignment3/test-claims-predictions.json'
 
 def CFEVER_main():
-    _, _, test_claims, evidences = load_data()
+    train_claims, dev_claims, test_claims, evidences = load_data()
 
-    net_er = er_pipeline()
+    net_er = er_pipeline(train_claims, dev_claims, evidences)
 
-    test_set = CFEVERERTestDataset(test_claims, evidences)
-    test_loader = DataLoader(test_set, batch_size=loader_batch_size, num_workers=loader_worker_num)
+    test_set_er = CFEVERERTestDataset(test_claims, evidences)
+    test_loader_er = DataLoader(test_set_er, batch_size=loader_batch_size, num_workers=loader_worker_num)
 
-    claim_evidences = predict_evi(net_er, test_loader, gpu)
-    test_claims = extract_er_result(claim_evidences, test_claims)
+    test_claims = extract_er_result(predict_evi(net_er, test_loader_er, gpu), test_claims)
+
+    test_set_clc = CFEVERLabelTestDataset(test_claims, evidences)
+    test_loader_clc = DataLoader(test_set_clc, batch_size=loader_batch_size, num_workers=loader_worker_num)
+
+    net_clc = clc_pipeline(train_claims, dev_claims, evidences)
+
+    claim_labels = decide_claim_labels(net_clc, test_loader_clc, gpu)
+    extract_claim_evi_labels(test_claims, claim_labels, output_filename)  # This function saves the final predictions to the destination file
