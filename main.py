f7c453394a1bd40963079f24022adcbe513d4aaf
zyinghua <yzho0149@student.monash.edu>
2023-05-08 23:12:22 +1000

    amended import methods due to circular import problem

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This file contains the code to run the pipeline of the Climate FEVER system.
 
-from bert_er import extract_er_result, predict_evi, er_pipeline, CFEVERERTestDataset
-from bert_clc import extract_claim_evi_labels, decide_claim_labels, clc_pipeline, CFEVERLabelTestDataset
+import bert_er as ber
+import bert_clc as bclc
 from dataset_loader import load_data
 from torch.utils.data import DataLoader
 from transformers import BertTokenizer
@@ -24,17 +24,21 @@ def CFEVER_main():
     train_claims, dev_claims, test_claims, evidences = load_data()
     bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-    net_er = er_pipeline(train_claims, dev_claims, evidences)
+    net_er = ber.er_pipeline(train_claims, dev_claims, evidences)
 
-    test_set_er = CFEVERERTestDataset(test_claims, evidences, bert_tokenizer)
+    test_set_er = ber.CFEVERERTestDataset(test_claims, evidences, bert_tokenizer)
     test_loader_er = DataLoader(test_set_er, batch_size=loader_batch_size, num_workers=loader_worker_num)
 
-    test_claims = extract_er_result(predict_evi(net_er, test_loader_er, gpu), test_claims)
+    test_claims = ber.extract_er_result(ber.predict_evi(net_er, test_loader_er, gpu), test_claims)
 
-    test_set_clc = CFEVERLabelTestDataset(test_claims, evidences)
+    test_set_clc = bclc.CFEVERLabelTestDataset(test_claims, evidences)
     test_loader_clc = DataLoader(test_set_clc, batch_size=loader_batch_size, num_workers=loader_worker_num)
 
-    net_clc = clc_pipeline(train_claims, dev_claims, evidences)
+    net_clc = bclc.clc_pipeline(train_claims, dev_claims, evidences)
 
-    claim_labels = decide_claim_labels(net_clc, test_loader_clc, gpu)
-    extract_claim_evi_labels(test_claims, claim_labels, output_filename)  # This function saves the final predictions to the destination file
+    claim_labels = bclc.decide_claim_labels(net_clc, test_loader_clc, gpu)
+    bclc.extract_claim_evi_labels(test_claims, claim_labels, output_filename)  # This function saves the final predictions to the destination file
+
+
+if __name__ == '__main__':
+    print('Start running the pipeline...')
